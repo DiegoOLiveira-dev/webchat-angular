ca7f0a97ac3f88d74698ad98290441b811eaac5e
Diego <diego@MacBook-Pro-de-Diego.local>
2022-09-12 20:45:08 -0300

    Criação do chat service, implementacao dos metodos, chamadada dos metodos no component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,103 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from './services/chat/chat.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+
+  public roomId!: string;
+  public messageText!: string;
+  public messageArray: {user: string, message: string}[] = [];
+
+  public phone!: string;
+  public currentUser: any;
+  public selectedUser: any;
+
+  public userList = [
+    {
+      id: 1,
+      name: 'Diego',
+      phone: '123456789',
+      image: 'assets/user/user-1.png',
+      roomId: {
+        2: 'room-1',
+        3: 'room-2',
+        4: 'room-3'
+      }
+    },
+    {
+      id: 2,
+      name: 'Diana',
+      phone: '123445678',
+      image: 'assets/user/user-2.png',
+      roomId: {
+        1: 'room-1',
+        3: 'room-4',
+        4: 'room-5'
+      }
+    },
+    {
+      id: 3,
+      name: 'Lucas',
+      phone: '123455678',
+      image: 'assets/user/user-3.png',
+      roomId: {
+        1: 'room-2',
+        2: 'room-4',
+        4: 'room-6'
+      }
+    },
+    {
+      id: 4,
+      name: 'Alef',
+      phone: '123456678',
+      image: 'assets/user/user-4.png',
+      roomId: {
+        1: 'room-3',
+        2: 'room-5',
+        3: 'room-6'
+      }
+    },
+  ]
+
+  constructor(
+    private chatService: ChatService
+  ) {
+
+    this.chatService.getMessage()
+      .subscribe((data: {user: string, message: string}) => {
+        this.messageArray.push(data);
+      })
+
+  }
+  ngOnInit(): void {
+
+    this.currentUser = this.userList[0]
+
+  }
+
+  selectUserHandler(phone: string):void {
+    this.selectedUser = this.userList.find(user => user.phone === phone)
+    this.roomId = this.selectedUser.roomId[this.selectedUser.id]
+    this.messageArray = []
+
+    this.join(this.currentUser.name, this.roomId)
+  }
+
+  join(userName: string, roomId: string): void {
+    this.chatService.jonRoom({user: userName, roomId })
+  }
+
+  sendMessage(): void {
+    this.chatService.sendMessage({
+      data: this.currentUser.name,
+      room: this.roomId,
+      message: this.messageText
+    })
+
+    this.messageText = ''
+  }
 }
--- frontend/src/app/services/chat/chat.service.ts
+++ frontend/src/app/services/chat/chat.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { io, Socket } from 'socket.io-client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  constructor() { }
+  private socket: Socket;
+  private url = 'http://localhost:3000'
+
+  constructor() {
+    this.socket = io(this.url)
+  }
+
+  jonRoom(data: any): void {
+    this.socket.emit('join', data);
+  }
+
+  sendMessage(data: any): void {
+    this.socket.emit('message', data)
+  }
+
+  getMessage(): Observable<any>{
+    return new Observable<{user: string, message: string}>(observer => {
+      this.socket.on('new message', (data) => {
+        observer.next(data);
+      })
+
+      return () => {
+        this.socket.disconnect()
+      }
+    })
+
+    
+  }
+
+
+
 }
